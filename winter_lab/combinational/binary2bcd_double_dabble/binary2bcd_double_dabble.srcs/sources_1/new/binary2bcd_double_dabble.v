e7815e4b0ed42a4f1f6a34f7d96bbb9e3b2c65ab
nina1032024 <yucenc4@gmail.com>
2025-02-05 14:37:35 +0800

    double dabble coding style

--- winter_lab/combinational/binary2bcd_double_dabble/binary2bcd_double_dabble.srcs/sources_1/new/binary2bcd_double_dabble.v
+++ winter_lab/combinational/binary2bcd_double_dabble/binary2bcd_double_dabble.srcs/sources_1/new/binary2bcd_double_dabble.v
@@ -21,25 +21,27 @@
 
 
 module binary2bcd_double_dabble(
-    input wire[7:0] in_binary,
-    output wire [15:0] unpacked_bcd,
-    output wire [7:0] packed_bcd
-);
+        input wire[7:0] in_binary,
+        output wire [15:0] unpacked_bcd,
+        output wire [7:0] packed_bcd
+    );
 
-reg [15:0] scratch_pad;
+    reg [15:0] scratch_pad;
 
-always @* begin
-    scratch_pad = {8'b00000000, in_binary};
-    for(integer i = 1; i < 8; i = i + 1) begin
+    always @*
+    begin
+        scratch_pad = {8'b00000000, in_binary};
+        for(integer i = 1; i < 8; i = i + 1)
+        begin
+            scratch_pad = scratch_pad << 1;
+            if (scratch_pad[11:8] > 4'b0100)
+                scratch_pad[11:8] = scratch_pad[11:8] + 4'b0011;
+        end
         scratch_pad = scratch_pad << 1;
-        if (scratch_pad[11:8] > 4'b0100)
-		    scratch_pad[11:8] = scratch_pad[11:8] + 4'b0011;
     end
-    scratch_pad = scratch_pad << 1;
-end
 
-assign packed_bcd =  scratch_pad[15:8];
-assign unpacked_bcd = {4'b0000, scratch_pad[15:12], 4'b0000, scratch_pad[11:8]};
+    assign packed_bcd =  scratch_pad[15:8];
+    assign unpacked_bcd = {4'b0000, scratch_pad[15:12], 4'b0000, scratch_pad[11:8]};
 
-endmodule 
+endmodule
 
