dbaccf28fc36e6ee3e29cd3e9ce920374e19e297
nina1032024 <yucenc4@gmail.com>
2025-02-05 17:07:59 +0800

    ignore file vscode

---
+++ winter_lab/sequential/uart_receiver/uart_receiver/uart_receiver.srcs/sim_1/new/test_uart_receiver.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2025/02/05 17:01:10
+// Design Name: 
+// Module Name: test_uart_receiver
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module test_uart_receiver(
+
+    );
+endmodule
---
+++ winter_lab/sequential/uart_receiver/uart_receiver/uart_receiver.srcs/sources_1/new/uart_receiver.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2025/02/05 17:00:41
+// Design Name: 
+// Module Name: uart_receiver
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module uart_receiver(
+
+    );
+endmodule
