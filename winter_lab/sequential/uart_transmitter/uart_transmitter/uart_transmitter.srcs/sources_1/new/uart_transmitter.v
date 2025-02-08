badc1ad6e5e250d50137ae9a82f37581b6c25ef9
nina1032024 <yucenc4@gmail.com>
2025-02-08 11:55:04 +0800

    transmitter

--- winter_lab/sequential/uart_transmitter/uart_transmitter/uart_transmitter.srcs/sources_1/new/uart_transmitter.v
+++ winter_lab/sequential/uart_transmitter/uart_transmitter/uart_transmitter.srcs/sources_1/new/uart_transmitter.v
@@ -1,24 +1,23 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 2025/02/07 14:54:42
-// Design Name: 
+// Company:
+// Engineer:
+//
+// Create Date: 2025/02/07 12:11:50
+// Design Name:
 // Module Name: uart_transmitter
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
+// Project Name:
+// Target Devices:
+// Tool Versions:
+// Description:
+//
+// Dependencies:
+//
 // Revision:
 // Revision 0.01 - File Created
 // Additional Comments:
-// 
+//
 //////////////////////////////////////////////////////////////////////////////////
-
 
 `define IDLE 0
 `define TRANSMIT 1
@@ -41,7 +40,7 @@ module uart_transmitter(
     reg [9:0] d;
 
     always @(*) begin
-        d = {1'b1, data, 1'b0};
+        d = {1'b1, data, 1'b0}; // Stop bit (1), Data (8 bits), Start bit (0)
     end
 
     always@* begin
@@ -49,7 +48,7 @@ module uart_transmitter(
             `IDLE: begin
                 if(start == 1) begin
                     next_state = `TRANSMIT;
-                    uart_tx_local = 1;
+                    uart_tx_local = 0; //start bit
                     next_bit_counter = 0;
                 end else begin
                     next_state = `IDLE;
@@ -61,18 +60,18 @@ module uart_transmitter(
                 if(baud_rate_signal == 1) begin
                     if(bit_counter == 9) begin
                         next_state = `IDLE;
-                        uart_tx_local = 1;
+                        uart_tx_local = 1; //stop bit
                         next_bit_counter = 0;
                     end else begin
                         next_state = `TRANSMIT;
-                        uart_tx_local = d[bit_counter];
+                        uart_tx_local = d[bit_counter]; //sending current bits
                         next_bit_counter = bit_counter +1;
                     end
                 end else begin
                     if(bit_counter == 0) begin
-                        uart_tx_local = 1;
+                        uart_tx_local = 1; 
                     end else begin
-                        uart_tx_local = d[bit_counter - 1];
+                        uart_tx_local = d[bit_counter - 1]; //transmit previous bit
                     end
                     next_state = `TRANSMIT;
                     next_bit_counter = bit_counter;
@@ -98,4 +97,3 @@ module uart_transmitter(
         end
     end
 endmodule
-
