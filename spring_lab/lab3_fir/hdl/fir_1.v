ef0715c3001fafd4d8c7b12492577f47a25d6b06
nina1032024 <yucenc4@gmail.com>
2025-03-14 14:32:44 +0800

    config_read_check

--- spring_lab/lab3_fir/hdl/fir_1.v
+++ spring_lab/lab3_fir/hdl/fir_1.v
@@ -1,12 +1,7 @@
-// fsm state
-`define IDLE 2'b00
-`define CALC 2'b01
-`define DONE 2'b10
-
 module fir
     #(  parameter pADDR_WIDTH = 12,
-        parameter pDATA_WIDTH = 32,
-        // 要定義sram 的長度
+        parameter pDATA_WIDTH = 32
+        // sram length
      )
      (
          // AXI4-Lite for configuration
@@ -23,7 +18,7 @@ module fir
          input   wire                     arvalid,  // read  address valid
          input   wire [(pADDR_WIDTH-1):0] araddr,   // read  address
          output  reg                      rvalid,   // read  data    valid
-         output  wire [(pDATA_WIDTH-1):0]  rdata,   // read  data
+         output  reg [(pDATA_WIDTH-1):0]  rdata,   // read  data
 
          // AXI4-Stream slave for x input, SS bus
          input   wire                     ss_tvalid,
@@ -52,38 +47,102 @@ module fir
          input   wire [(pDATA_WIDTH-1):0] data_Do,
 
          input   wire                     axis_clk,
-         input   wire                     axis_rst_n
+         input   wire                     axis_rst_n,
+
+         output [(pADDR_WIDTH-1):0] araddr_latch
      );
 
     reg [2:0] ap_ctrl;
+
+// FSM states
+    localparam IDLE = 2'b00;
+    localparam CALC = 2'b10;
+    localparam DONE = 2'b11;
+
+    localparam RIDLE  = 1'b0;
+    localparam RVALID = 1'b1;
 
 // axi-lite interface : write channel, read channel
     wire awready_tmp;
     wire wready_tmp;
     wire arready_tmp;
-    wire rvalid_tmp;
+    //reg [(pDATA_WIDTH-1):0] rdata_tmp;
 
     always @(posedge axis_clk or negedge axis_rst_n) begin
         if (!axis_rst_n) begin
             awready <= 1'b0;
             wready  <= 1'b0;
             arready <= 1'b0;
-            rvalid  <= 1'b0;
         end else begin
-            awready <= awready_tmp;    // awready = 1 only for one cycle
-            wready  <= wready_tmp;     // wready  = 1 only for one cycle
-            arready <= arready_tmp;    // arready = 1 only for one cycle
-            rvalid  <= rvalid_tmp;     // rvalid  = 1 only for one cycle
+            awready <= awready_tmp;    
+            wready  <= wready_tmp;     
+            arready <= arready_tmp;      
         end
     end
 
-    assign awready_tmp = wvalid && awvalid & (~awready);
-    assign wready_tmp  = awvalid && wvalid  & (~wready); 
-    assign arready_tmp = arvalid & (~arready);
-    assign rvalid_tmp  = rready  & (~rvalid); 
-    // 要先給address(arrvalid, rvalid)，才能用rvalid 
+    // axilite write
+        // synchronization of address and data 
+        // signal only last for one cycle
+    assign awready_tmp = awvalid && wvalid  & (~wready); 
+    assign wready_tmp  = awvalid && wvalid  & (~wready);
+
+    // axilite read
+        // first get address then return data
+        // signal only last for one cycle
+    assign arready_tmp = arvalid && (~arready);
+
+    // latch rvalid till rready (use fsm)
+    reg read_state, next_read_state;
+    reg next_rvalid;
+    always@* begin
+        case(read_state)
+            RIDLE: begin
+                if(arvalid && arready) begin
+                    next_read_state = RVALID;
+                    next_rvalid = 1;
+                end else begin
+                    next_read_state = RIDLE;
+                    next_rvalid = 0;
+                end
+            end
+            RVALID: begin
+                if(rready) begin
+                    next_read_state = RIDLE;
+                    next_rvalid = 0;
+                end else begin
+                    next_read_state = RVALID;
+                    next_rvalid = 1;
+                end
+            end
+        endcase
+    end
     
-    assign rdata = (araddr == 12'h00) ? ap_ctrl : tap_Do; // 會讀到
+    always@(posedge axis_clk or negedge axis_rst_n) begin
+        if(!axis_rst_n) begin
+            read_state <= RIDLE;
+            rvalid <= 1'b0;
+        end else begin
+            read_state <= next_read_state;
+            rvalid <= next_rvalid;
+        end
+    end
+
+    // latch araddr
+    wire [(pADDR_WIDTH-1):0] araddr_latch;
+    assign araddr_latch = (arvalid && arready) ? araddr : araddr_latch;
+
+    // read data contains read data_len, tap_num, coefficient, state
+    always @* begin
+        if(rvalid) begin
+            if(araddr_latch == 12'h00) rdata = ap_ctrl;
+            else if (araddr_latch == 12'h10) rdata = data_length;
+            else if (araddr_latch == 12'h14) rdata = tap_num;
+            else if (araddr_latch >=  12'h80 && araddr_latch <= 12'hFF) rdata = tap_Do;
+            else rdata = 12'hFFFFFFFF;
+        end else begin
+            rdata = 12'hFFFFFFFF;
+        end
+    end
 
 // configuration register
     // FSM for fir operation
@@ -91,32 +150,32 @@ module fir
 
     always@* begin
         case(state)
-            `IDLE : begin
+            IDLE : begin
                 if(awaddr == 12'h00 && wdata[0] == 1'b1 && awvalid && wvalid) begin    // ap_start = 1
-                    next_state = `CALC;
+                    next_state = CALC;
                 end else begin
-                    next_state = `IDLE;
+                    next_state = IDLE;
                 end
             end
-            `CALC : begin
-                if(sm_tvalid && sm_tlast && ss_tlast) begin     // ss_tlast 不用放在這裡       // transfer last data of y
-                    next_state = `DONE;
+            CALC : begin
+                if(sm_tvalid && sm_tlast) begin                                       // transfer last data of y
+                    next_state = DONE;
                 end else begin
-                    next_state = `CALC;
+                    next_state = CALC;
                 end
             end
-            `DONE : begin
+            DONE : begin
                 if(araddr == 12'h00 && arvalid && rvalid && rready && arready) begin  // read address 0x00
-                    next_state = `IDLE;
+                    next_state = IDLE;
                 end else begin
-                    next_state = `DONE;
+                    next_state = DONE;
                 end
             end
             default : begin
                 if(awvalid && wvalid && (awaddr == 12'h10 || awaddr == 12'h14)) begin
-                    next_state = `CALC;
+                    next_state = CALC;
                 end else begin
-                    next_state = `IDLE;
+                    next_state = IDLE;
                 end
             end
         endcase
@@ -124,7 +183,7 @@ module fir
 
     always@(posedge axis_clk or negedge axis_rst_n) begin
         if(!axis_rst_n) begin
-            state <= `IDLE;
+            state <= IDLE;
         end else begin
             state <= next_state;
         end
@@ -133,11 +192,11 @@ module fir
     // 0x00: ap_ctrl = {ap_idle, ap_done, ap_start}
 
     // 要用sequential 
-    always @(posedge clk or negedge rst) begin
+    always @(posedge axis_clk or negedge axis_rst_n) begin
 
         ap_ctrl[0] = (awaddr == 12'h00 && wdata [0] == 1'b1 && awvalid && wvalid && awready && wready) ? 1'b1 : 1'b0;
-        ap_ctrl[1] = (state == `DONE) ? 1'b1 : 1'b0;
-        ap_ctrl[2] = (state == `IDLE) ? 1'b1 : 1'b0;
+        ap_ctrl[1] = (state == DONE) ? 1'b1 : 1'b0;
+        ap_ctrl[2] = (state == IDLE) ? 1'b1 : 1'b0;
     end
 
     // 0x10-14: data_length
@@ -172,7 +231,7 @@ module fir
     always@(posedge axis_clk or negedge axis_rst_n) begin
         if(!axis_rst_n) begin
             tap_cnt <= tap_num;
-        end else if(state != `CALC)begin
+        end else if(state != CALC)begin
             tap_cnt <= tap_num;
         end else begin
             tap_cnt <= (tap_cnt + 1 < tap_num) ? tap_cnt + 1 : 0;
@@ -181,10 +240,10 @@ module fir
 
     // read or write tap parameters
     always@* begin
-        if ((awaddr[11:0] >= 12'h80 && awaddr[11:0] <= 12'hFF) && (awvalid && wvalid)) begin
+        if ((awaddr[11:0] >= 12'h80 && awaddr[11:0] <= 12'hFF) && (awvalid && wvalid && awready && wready)) begin
             tap_A_w_r = awaddr[6:0];
-        end else if ((araddr[11:0] >= 12'h80 && araddr[11:0] <= 12'hFF) && (arvalid && rready)) begin
-            tap_A_w_r = araddr[6:0];
+        end else if ((araddr_latch[11:0] >= 12'h80 && araddr_latch[11:0] <= 12'hFF)) begin
+            tap_A_w_r = araddr_latch[6:0];
         end else begin
             tap_A_w_r = 12'hFFF;
         end
@@ -196,7 +255,7 @@ module fir
         tap_WE = ((awvalid && wvalid && awready && wready) &&
                   (awaddr[11:0] >= 12'h80 && awaddr[11:0] <= 12'hFF)) ? 4'b1111 : 4'b0000;
         tap_Di = (awvalid && wvalid) ? wdata : 12'h00;
-        tap_A = (state == `CALC) ? 4 * tap_cnt : tap_A_w_r;
+        tap_A = (state == CALC) ? 4 * tap_cnt : tap_A_w_r;
     end
 
 // axi-stream for x input
@@ -211,7 +270,7 @@ module fir
         end
     end    
 
-    assign ss_tready_tmp = (state == `CALC) && ss_tvalid && (tap_cnt == tap_num - 1);
+    assign ss_tready_tmp = (state == CALC) && ss_tvalid && (tap_cnt == tap_num - 1);
 
 
 // bram for data RAM
@@ -230,7 +289,7 @@ module fir
     always@ (posedge axis_clk or negedge axis_rst_n) begin
         if(!axis_rst_n) begin
             x_w_cnt <= tap_num - 1;
-        end else if (state == `IDLE) begin
+        end else if (state == IDLE) begin
             x_w_cnt <= tap_num - 1;
         end else begin 
             x_w_cnt <= x_w_cnt_tmp;
@@ -258,8 +317,8 @@ module fir
     // bram signal (Note : initialize before axi stream input)
     always@* begin
         data_EN = 1'b1;
-        data_WE = (state == `CALC && ss_tvalid && ss_tready || state == `IDLE) ? 4'b1111 : 4'b0000; 
-        data_Di = (state == `CALC && ss_tvalid && ss_tready) ? ss_tdata : 32'h00;
+        data_WE = (state == CALC && ss_tvalid && ss_tready || state == IDLE) ? 4'b1111 : 4'b0000; 
+        data_Di = (state == CALC && ss_tvalid && ss_tready) ? ss_tdata : 32'h00;
         data_A =  (ss_tvalid && ss_tready) ? 4 * x_w_cnt : 4 * x_r_cnt;
     end
 
@@ -280,7 +339,7 @@ module fir
     always@ (posedge axis_clk or negedge axis_rst_n) begin
         if(!axis_rst_n) begin
             mul <= 0;
-        end else if(state == `IDLE) begin
+        end else if(state == IDLE) begin
             mul <= 0;
         end else begin
             mul <= mul_tmp;
@@ -291,7 +350,7 @@ module fir
     always@ (posedge axis_clk or negedge axis_rst_n) begin
         if(!axis_rst_n) begin
             y <=  0;
-        end else if(state == `IDLE) begin
+        end else if(state == IDLE) begin
             y <= 0;
         end else if (tap_cnt == 2) begin
             y <= mul;
@@ -308,7 +367,7 @@ module fir
     always @(posedge axis_clk or negedge axis_rst_n) begin
         if(!axis_rst_n) begin
             y_cnt <= 0;
-        end else if(state == `IDLE) begin
+        end else if(state == IDLE) begin
             y_cnt <= 0;
         end else if(tap_cnt == 2) begin
             y_cnt <= y_cnt + 1;
@@ -325,7 +384,7 @@ module fir
     always@(posedge axis_clk or negedge axis_rst_n) begin
         if(!axis_rst_n) begin
             sm_tlast <= 1'b0;
-        end else if(state == `IDLE) begin
+        end else if(state == IDLE) begin
             sm_tlast <= 1'b0;
         end else begin
             sm_tlast <= sm_tlast_tmp;
@@ -340,7 +399,7 @@ module fir
         if(!axis_rst_n) begin
             sm_tdata <= 32'h00;
             sm_tvalid <= 1'b0;
-        end else if(state == `IDLE) begin
+        end else if(state == IDLE) begin
             sm_tdata <= 32'h00;
             sm_tvalid <= 1'b0;
         end else begin
